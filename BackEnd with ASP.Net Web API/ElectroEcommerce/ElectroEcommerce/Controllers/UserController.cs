2d30a4c4a145f691e4445499bd4c2631038f7069
Knyaz <qnyazface@mail.ru>
2024-01-12 15:48:47 +0400

    get user by email and remove user action added

--- BackEnd with ASP.Net Web API/ElectroEcommerce/ElectroEcommerce/Controllers/UserController.cs
+++ BackEnd with ASP.Net Web API/ElectroEcommerce/ElectroEcommerce/Controllers/UserController.cs
@@ -38,20 +38,50 @@ public class UserController : ControllerBase
 	[HttpGet("get-user-name/{name}")]
 	public async Task<ActionResult<User>> Get(string name)
 	{
-		var user= await _dataContext.Users.FirstOrDefaultAsync(u => u.Name.ToLower() == name.ToLower());
+		var user = await _dataContext.Users.FirstOrDefaultAsync(u => u.Name.ToLower() == name.ToLower());
 
 		if (user == null) { return NotFound("Username is not found"); }
 		return Ok(user);
 	}
+
+	[HttpGet("get-user-email/{email}")]
+	public async Task<ActionResult<User>> GetUserByEmail(string email)
+	{
+		var user = await _dataContext.Users.FirstOrDefaultAsync(u => u.Email.ToLower() == email.ToLower());
+		if (user == null) { return NotFound("this email not found"); }
+		return Ok(user);
+	}
+
+
 
 
 
 	[HttpPost("add-user")]
 	public async Task<ActionResult<List<User>>> AddUser(User user)
 	{
+		user.Id = Guid.NewGuid();
+
 		_dataContext.Users.Add(user);
+
 		await _dataContext.SaveChangesAsync();
+
 		return Ok(await _dataContext.Users.ToListAsync());
+	}
+
+
+	[HttpDelete("remove-user/{id}")]
+	public async Task<ActionResult<User>> Delete(Guid id)
+	{
+		var user = await _dataContext.Users.FindAsync(id);
+
+		if (user == null) { return NotFound("USer not found"); }
+
+		_dataContext.Users.Remove(user);
+
+		await _dataContext.SaveChangesAsync();
+
+		return Ok(user);
+
 	}
 
 
