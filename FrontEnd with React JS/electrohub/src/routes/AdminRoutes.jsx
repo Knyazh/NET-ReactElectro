d166bfd4369e4ed3c19441229917abb663adf021
Knyaz <qnyazface@mail.ru>
2024-01-31 16:45:08 +0400

    fix admin routes

--- FrontEnd with React JS/electrohub/src/routes/AdminRoutes.jsx
+++ FrontEnd with React JS/electrohub/src/routes/AdminRoutes.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import AdminIndex from '../admin/pages/Index/AdminIndex'
+import AdminLayout from '../layout/AdminLayout'
+import Category from '../admin/pages/Categories/Category'
+import Products from '../admin/pages/Products/Products'
+import Colors from '../admin/pages/Colors/Colors'
 
 const AdminRoutes = () => {
   return (
-    <div>AdminRoutes</div>
+       <AdminLayout>
+      <Routes>
+        <Route path='/index' element={<AdminIndex/>}/>
+        <Route path='/categories' element={<Category/>}/>
+        <Route path='/products' element={<Products/>}/>
+        <Route path='/colors' element={<Colors/>}/>
+      </Routes>
+    </AdminLayout>
   )
 }
 
