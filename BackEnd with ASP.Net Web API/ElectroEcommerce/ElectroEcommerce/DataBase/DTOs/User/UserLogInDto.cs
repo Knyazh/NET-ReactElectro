1d3884904afa97d5405413f29d4bd970485913b0
Knyaz <qnyazface@mail.ru>
2024-02-21 16:33:32 +0400

    email and Password validators added

--- BackEnd with ASP.Net Web API/ElectroEcommerce/ElectroEcommerce/DataBase/DTOs/User/UserLogInDto.cs
+++ BackEnd with ASP.Net Web API/ElectroEcommerce/ElectroEcommerce/DataBase/DTOs/User/UserLogInDto.cs
@@ -6,9 +6,9 @@ public class UserLogInDto
 {
 
 	[EmailAddress(ErrorMessage = "Invalid email format! Please enter a valid email address...")]
-	[RegularExpression(pattern: @"^[a-zA-Z0-9._%+-]{1,64}@[a-zA-Z0-9.-]{1,255}\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid email format! Please enter a valid email address...")]
+
 	public string Email { get; set; } = string.Empty;
 	[Required(ErrorMessage = "Password field cannot be left blank! Please fill in the relevant field...")]
-	[StringLength(100, MinimumLength = 6, ErrorMessage = "Password length must be minimum 6 character")]
+	
 	public string Password { get; set; } = string.Empty;
 }
---
+++ BackEnd with ASP.Net Web API/ElectroEcommerce/ElectroEcommerce/Validators/UserLoginValidator.cs
@@ -0,0 +1,18 @@
+﻿using ElectroEcommerce.DataBase.DTOs.User;
+using FluentValidation;
+
+namespace ElectroEcommerce.Validators;
+
+public class UserLoginValidator : AbstractValidator<UserLogInDto>
+{
+	public UserLoginValidator()
+	{
+		RuleFor(x => x.Password)
+			.NotEmpty().WithMessage("You have to fill this blank.")
+			.MinimumLength(6).WithMessage("Password length must be minimum 6.")
+			.Matches("^[a-zA-Z0-9]+$").WithMessage("The entered password must consist of only uppercase, lowercase letters, and numbers!");
+		RuleFor(x => x.Email)
+			.NotEmpty().WithMessage("You have to fill this blank.")
+			.EmailAddress().WithMessage("Invalid email format.");
+	}
+}
